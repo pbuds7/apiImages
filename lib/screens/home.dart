3d13c3a2e8927731b55c4e2fa0dbb5948ef37a66
Francisco Burgmeijer <franciscoburgmeijer@gmail.com>
2023-01-19 09:31:27 -0500

    Succesful API call

---
+++ lib/screens/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Consumo de API Desarrollo Móvil'),
+      ),
+      body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user = users[index];
+            final email = user['email'];
+            return ListTile(title: Text(email));
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchUsers,
+      ),
+    );
+  }
+
+  void fetchUsers() async {
+    print('fetchUsers called');
+    const url = 'https://randomuser.me/api/?results=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+    print('fetchUsers completed');
+  }
+}
